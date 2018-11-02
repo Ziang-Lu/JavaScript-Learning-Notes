6d47f5da4d512c7ec74003f2c6ff53e24c3e3e04
Ziang Lu <ziangl@alumni.cmu.edu>
2018-11-01 19:52:13 -0700

    Finished basic data types

---
+++ basic_data_types.js
@@ -0,0 +1,36 @@
+// 1. number
+console.log('Type of 123: ' + typeof(123)); // Type of 123: number
+console.log('2 / 0 = ' + 2 / 0); // 2 / 0 = Infinity
+console.log('0 / 0 = ' + 0 / 0); // 0 / 0 = NaN
+
+// 2. boolean
+console.log('Type of true: ' + typeof(true)); // Type of true: boolean
+
+// 3. string
+console.log("Type of 'any text': " + typeof('any text')); // Type of 'any text': string
+
+// 4. array
+console.log('Type of an array: ' + typeof([123, true, 'any text'])); // Type of an array: object
+
+// Special Data Values
+// null: "Value of nothing or totally empty"
+var x = null;
+console.log(null); // null
+// undefined: "Absence of a value, not even a value of null"
+var y;
+console.log(y); // undefined
+
+// Implicit Type Coercion
+console.log('Hello' + 5 * 10); // Hello50
+// Evaluate 5 * 10 to 50, implicitly convert it to a string '50', and
+// concatenate it to 'Hello', resuling in 'Hello50'
+/*
+Note that when performing comparison using == and !=, first implicit type
+coercion is done, and then to perform the comparison
+This may lead to quirky results,
+e.g., '1' ==  true -> true,
+so this is considered to be a BAD PRACTICE.
+Thus, to perform comparisons, we should use === and !==  for strict comparison,
+which checks both the type and the value of the two operands,
+e.g., '1' === true -> false
+*/
--- first_script.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello, world!");
-
-for (var i = 0; i < 10; ++i) {
-    console.log(i);
-}
