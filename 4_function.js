96153e6aebf97e1f317d43d368aff02835cf5504
Ziang Lu <ziangl@alumni.cmu.edu>
2018-11-03 01:46:05 -0700

    Add function hoisting

--- 4_function.js
+++ 4_function.js
@@ -18,6 +18,67 @@ console.log(
 // Note that in a function, if we don't explicit return a value, the default
 // return value is 'undefined'
 function func() {
-  console.log('here');
+  console.log("This function doesn't return anything.");
 }
 console.log(func()); // undefined
+
+
+// ********** HOISTING **********
+
+let avg = calcAvg(5, 9); // Call a function before its declaration (definition)
+// Is it possible?
+
+function calcAvg(a, b) {
+  return (a + b) / 2;
+}
+
+console.log(avg); // 7
+// It is possible!
+
+/*
+This JavaScript feature is called "hoisting".
+In JavaScript, before the codes are executed, all function declarations
+(definition) are "hoisted" to the top of the current scope, so that it can be
+used later in the program.
+i.e., In the example above, before the codes are executed, JavaScript will
+automatically "hoist" the declaration (definition) of the function "calcAvg()"
+to the top of the global scope, and then execute the codes, so that the above
+codes are in effect rearranged by JavaScript to:
+
+function calAvg(a, b) {
+  return (a + b) / 2;
+}
+
+let avg = calcAvg(5, 9);
+console.log(avg);
+ */
+
+// "Hoisting" happens for variables, too.
+
+function greet() {
+  console.log(greeting); // In a function, use a variable before its declaration
+  var greeting = 'Hello!';
+  // Is it possible?
+
+  // Output: undefined
+  // It is possible!
+}
+
+greet();
+
+/*
+In the above example, before the codes are executed, JavaScript will
+automatically "hoist" the declaration of the variable "greeting" to the top of
+the function scope, and then execute the codes, so that the above codes are in
+effect rearranged by JavaScript to:
+
+function greet() {
+  var greeting;
+  console.log(greeting);
+  greeting = 'Hello!';
+}
+ */
+
+ // Note that the "hositing" for variables is very similar the C-style coding
+ // convention, which means in each function, first declare all the variables,
+ // and then start using them
